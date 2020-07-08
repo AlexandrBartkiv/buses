79bd39322e9c0fb520528b2c3951fc072341c748
AlexandrBarkiv <alexandrbartkiv@gmail.com>
2020-07-08 17:58:53 +0300

    autocompleate changed

--- index.js
+++ index.js
@@ -1,11 +1,114 @@
 $( document ).ready(function(){
 console.log('hi')
-$('#from').autocomplete({
-    source: ["Киев","Львов","Одесса"]
-})
-$('#to').autocomplete({
-    source: ["Киев","Львов","Одесса"]
-})
+// $('#from').autocomplete({
+//     source: ["Киев","Львов","Одесса"]
+// })
+// $('#to').autocomplete({
+//     source: ["Киев","Львов","Одесса"]
+// })
+
+
+inp = document.querySelector('#from')
+function autocomp(inp, arr) {
+
+  var currentFocus;
+
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      arr = ["Киев","Львов","Одесса"];
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+
+      this.parentNode.appendChild(a);
+
+      for (i = 0; i < arr.length; i++) {
+
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+          b = document.createElement("DIV");
+
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+              b.addEventListener("click", function(e) {
+
+              inp.value = this.getElementsByTagName("input")[0].value;
+
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+
+        currentFocus++;
+
+        addActive(x);
+      } else if (e.keyCode == 38) { 
+
+        currentFocus--;
+   
+        addActive(x);
+      } else if (e.keyCode == 13) {
+
+        e.preventDefault();
+        if (currentFocus > -1) {
+
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    
+    if (!x) return false;
+    
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+
+autocomp(inp);
+
+
+
+
+
+
+
+
 
 $('#datepicker').datepicker($.extend({
     inline: true,
